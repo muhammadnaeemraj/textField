f1515933b15f0af29eceabf1ecd89fc74ea8f2fa
Muhammad Naeem Raj <90338269+muhammadnaeemraj@users.noreply.github.com>
2022-11-19 19:52:14 +0000

    simple text field commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text , setText] = useState('Hi');
+  let handleChange=(e)=>{
+    setText(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{text}</h1>
+      <input onChange={handleChange}/>
+      <button onClick={()=>{setText('Hi')}}>Reset</button>
     </div>
   );
 }
